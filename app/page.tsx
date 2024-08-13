e35d02e2c5157a70543162f38aacd2466c5b2ee3
meng <as2980000@gmail.com>
2024-08-13 20:52:51 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+
 import { Heading1 } from "lucide-react";
 import Image from "next/image";
 import { Fragment } from "react";
@@ -25,15 +26,45 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel"
 import { CarouselSize } from "./test";
+import Tee from "../pages/pee";
 
 
-export default function Home() {
+import { createClient } from "@libsql/client";
 
-  Fragment
+export const turso = createClient({
+  url: "libsql://jp-actor-meng.turso.io",
+  authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJleHAiOjE3MjQwMjU2MDIsImlhdCI6MTcyMzQ2ODE0MSwiaWQiOiI2YjFkMmE1MC1hYTkzLTRlODUtOWIzNC1jOGI1ODJmMmRhZGMifQ.Wu2pegQbyEDexfVGX0d_wGFHSgyc7kmUgUWe5DeldtTF0DIHlHWCntx4OccBWejJsPoVhyNzrUOGp-ocNzIrAw",
+});
+
+export const turso2 = createClient({
+  url: process.env.TURSO_DATABASE_URL?process.env.TURSO_DATABASE_URL:"libsql://jp-actor-meng.turso.io",
+  authToken: process.env.TURSO_AUTH_TOKEN,
+});
+
+async function testDb(){
+
+console.log(process.env.TURSO_DATABASE_URL);
+
+ const ac=await turso.execute(" SELECT * FROM actors");
+
+ //var n=ac.rows[0]["ch_name"]
+ //console.log(n)
+ console.log(ac);
+
+ console.log(ac.rows)
+ return ac.rows;
+}
+
+export   default function Home() {
+
+ const rows= testDb();
+
+ 
+
   return (
     <div>
 
-
+    <h1></h1>
     <CarouselSize></CarouselSize>
 
 
---
+++ pages/doc.tsx
@@ -0,0 +1,6 @@
+import { Card } from "@material-tailwind/react";
+
+export default function Test(){
+
+    return  (<h2>test</h2>);
+}
